162077f39c7f2f66150685b9d7a1ef1894d4d997
Shreyas Gavhalkar <shreyas@pop-os.localdomain>
2023-02-16 18:18:09 +0530

    created basic skeleton of database.py

--- database.py
+++ database.py
@@ -1,16 +1,21 @@
-def get_o_auth(email,password):
+
+def authenticate(email, password):
+    #check if user exists, and sign them in
+    #return true if successful else return NONE
+
+def create_user(email, password):
+    #check if email already exists, if yes return NONE else return true
+
+def create_contiguous_booking(email, count, time):
+    #TODO: write booking algorithm
+    #if can book seats mutex lock, and return table(s) allocated else return NONE
     return None
 
-def create_user(email,password):
-    return {"status": "success"}
-
-
-def create_contiguous_booking(count):
-
+def create_order(email, items, time):
+    #place order, and return orderID
+    #TODO: algorithm to create uinque order id(seq numbers)
     return None
 
-def create_order(a,b):
-    return None
-
-def get_menu():
+def get_menu(canteen_id=1):
+    #return menu items list with price
     return None
