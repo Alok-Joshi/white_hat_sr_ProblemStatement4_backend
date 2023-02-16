11e635be1e52b66239fa554a1fbed3f8ff7260c1
Alok Joshi <AlokJoshi523@gmail.com>
2023-02-17 01:13:49 +0530

    implemented skeleton of admin endpoints and mongodb change streams

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Header, HTTPException, Depends
+from fastapi import FastAPI, Header, HTTPException, Depends, WebSocket
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 import jwt
@@ -17,7 +17,8 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-SECRET_KEY = ""
+USER_SECRET_KEY = "usercanteenbooking"
+ADMIN_SECRET_KEY = "adminusercanteenbooking"
 EXPIRY_TIME = 60 * 24 * 7
 
 
@@ -30,20 +31,36 @@ class order(BaseModel):
 
 class booking(BaseModel):
     seat_count: int
-    order: list[order]
+    orders: list[order]
 
+
+class menu_item(BaseModel):
+    item_name: str
+    item_price : int
 
 
 def get_jwt_token(email: str):
     return jwt.encode({ "email" : email ,
        'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes= EXPIRY_TIME)
-    }, SECRET_KEY, )
+    }, USER_SECRET_KEY, )
 
 def check_jwt_token(token : str = Header(None)):
     try:
-        return jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
+        return jwt.decode(token, USER_SECRET_KEY, algorithms=["HS256"])
     except:
         raise HTTPException(status_code=404, detail="JWT_TOKEN_NOT_FOUND")
+
+def get_jwt_admin_token(email: str):
+    return jwt.encode({ "email" : email ,
+       'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes= EXPIRY_TIME)
+    }, ADMIN_SECRET_KEY, )
+
+def check_jwt_admin_token(token : str = Header(None)):
+    try:
+        return jwt.decode(token, ADMIN_SECRET_KEY, algorithms=["HS256"])
+    except:
+        raise HTTPException(status_code=404, detail="JWT_TOKEN_NOT_FOUND")
+
 
 
 
@@ -67,7 +84,7 @@ def register(pl : payload):
         new_user_result = database.create_user(pl.email,pl.password)
         if(new_user_result is not None):
                 new_jwt_token = get_jwt_token(pl.email)
-                return { "message" : "User was registered Succesfully","token" : get_jwt_token(pl.email) }
+                return { "message" : "User was registered Succesfully","token" : new_jwt_token}
                 
         else:
                 raise HTTPException(status_code=404, detail="User Already exists")
@@ -85,11 +102,11 @@ def get_bookings():
 @app.post("/booking", dependencies=[Depends(check_jwt_token)],status_code = 200)
 def book_table(bk: booking,token: str = Header(None)):
 
-    booking_result = database.create_contiguous_booking(bk.seat_count);
+    email = jwt.decode(token,options = {"verify_signature":False})["email"]
+    booking_result = database.create_contiguous_booking(email,bk.seat_count);
     if(booking_result is not None):
         #create the order
-        email = jwt.decode(token,options = {"verify_signature":False})["email"]
-        order_creation_result = database.create_order(email,booking.order)
+        order_creation_result = database.create_order(email,booking.orders)
 
         if(order_creation_result is not None):
             #TODO: Return a  dictionary denoting success
@@ -106,11 +123,79 @@ def book_table(bk: booking,token: str = Header(None)):
 
 #MENU Endpoints
 
-@app.get("/menu",dependencies= [Depends(check_jwt_token)],status_code = 200)
-def get_menu():
+@app.get("/menu/{canteen_id}",dependencies= [Depends(check_jwt_token)],status_code = 200)
+def get_menu(canteen_id:int):
 
-    menu = database.get_menu();
+    menu = database.get_menu(canteen_id)
     return menu
+
+
+@app.post("/menu/{canteen_id}",dependencies= [Depends(check_jwt_admin_token)],status_code = 200)
+def add_item(mu: menu_item,canteen_id:int):
+    pass
+
+@app.put("/menu/{canteen_id}",dependencies= [Depends(check_jwt_admin_token)],status_code = 200)
+def update_item(mu: menu_item,canteen_id:int):
+    pass
+
+#@app.("/menu/{canteen_id}",dependencies= [Depends(check_jwt_admin_token)],status_code = 200)
+#def update_item(mu: menu_item,canteen_id:int):
+#    pass
+
+
+#LISTENERS 
+@app.websocket("/ws/notifications/orderstatus/{order_id}")
+async def order_status(websocket: WebSocket, order_id: str):
+
+    await websocket.accept()
+    while True:
+        client_data = await websocket.receive_text()
+        modified_order_document = database.check_order_document(order_id)
+        modified_order_document_string = json.dumps(modified_order_document)
+        await websocket.send_text(modified_order_document)
+
+
+@app.websocket("/ws/notifications/neworders/{canteen_id}")
+async def new_order(websocket: WebSocket):
+
+    await websocket.accept()
+    while True:
+        client_data = await websocket.receive_text()
+        new_ord = database.check_new_order()
+        new_ord_string = json.dumps(new_ord)
+        await websocket.send_text(new_ord_string)
+        
+
+
+#ADMIN ENDPOINTS
+
+@app.post("/admin/login",status_code = 200)
+def admin_login(pl: payload):
+    if pl.password == None:
+        raise HTTPException(status_code=404, detail="No Password Provided")
+    else:
+        auth_success = database.authenticate_admin(pl.email,pl.password)
+        if auth_success is not None: 
+                new_jwt_token = get_jwt_admin_token(pl.email)
+                return {"message" : "Logged In Succesfully", "token" : new_jwt_token}
+        else:
+                raise HTTPException(status_code=404, detail="User Not Found")
+
+
+@app.post("/admin/register", status_code = 200)
+def admin_register(pl : payload):
+    if pl.password != None:
+
+        new_user_result = database.create_admin(pl.email,pl.password)
+        if(new_user_result is not None):
+                new_jwt_token = get_jwt_admin_token(pl.email)
+                return { "message" : "User was registered Succesfully","token" : new_jwt_token}
+                
+        else:
+                raise HTTPException(status_code=404, detail="User Already exists")
+
+    else:
+        raise HTTPException(status_code = 404, detail = "No password entered")
 
 
 
