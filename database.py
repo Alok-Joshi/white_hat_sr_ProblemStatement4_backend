efe1e56e0f857585ebdf197039069d58bc2cdcb5
Shreyas Gavhalkar <shreyas@pop-os.localdomain>
2023-02-16 19:40:29 +0530

    implemented auth

--- database.py
+++ database.py
@@ -1,11 +1,26 @@
+import pymongo
+client = pymongo.MongoClient('mongodb+srv://shreyas22010793:A1Hn12yTvIvx21fw@cluster0.8dvqlye.mongodb.net/?retryWrites=true&w=majority')
+db = client.test2DB
 
-def authenticate(email, password):
+
+def authenticate(email="xyz@gmail.com", password="password"):
     #check if user exists, and sign them in
     #return true if successful else return NONE
+    user = db.user
+    res = user.find_one({"email":email})
+    #print(res)
+    if res is not None:
+        pswd = res['password']
+        if pswd==password:
+            return True
+        else:
+            return None
+    return res
 
 def create_user(email, password):
     #check if email already exists, if yes return NONE else return true
-
+    user = db.user
+    pass
 def create_contiguous_booking(email, count, time):
     #TODO: write booking algorithm
     #if can book seats mutex lock, and return table(s) allocated else return NONE
@@ -19,3 +34,6 @@ def create_order(email, items, time):
 def get_menu(canteen_id=1):
     #return menu items list with price
     return None
+
+
+print(authenticate())
