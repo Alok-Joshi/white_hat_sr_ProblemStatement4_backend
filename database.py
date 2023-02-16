e5124369429255b644d95553a426276c0d3f5a2d
Alok Joshi <AlokJoshi523@gmail.com>
2023-02-16 16:16:22 +0530

    database skeleton added

---
+++ database.py
@@ -0,0 +1,16 @@
+def get_o_auth(email,password):
+    return None
+
+def create_user(email,password):
+    return {"status": "success"}
+
+
+def create_contiguous_booking(count):
+
+    return None
+
+def create_order(a,b):
+    return None
+
+def get_menu():
+    return None
